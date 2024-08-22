be9fb3c1c59cd4e04a50eefd8564737e8b5b95d6
lee lu <nobody@nowhere.com>
2024-08-22 17:57:55 +0800

    no error

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -6,20 +6,12 @@ import { FlutterLoader } from '@/flutter'
 export default function Index() {
   useLoad(async () => {
     console.log('Page loaded.');
-    // if (process.env.TARO_ENV === 'weapp') {
-    //   const m = await import("@/canvaskit/pages/init.weapp");
-    //   await m.default();
-    // } else {
-    //   const m = await import("@/canvaskit/pages/init");
-    //   await m.default();
-    // }
     const m = await import("@/canvaskit/pages/init");
     await m.default();
     (window._flutter ??= {}).loader ??= new FlutterLoader();
     window._flutter.loader.load({
       onEntrypointLoaded: async (init) => {
         const host = $('#host').get(0)
-        host.append($('<div>item</div>'))
         const runner = await init.initializeEngine({
           assetBase: '/',
           fontFallbackBaseUrl: '/assets/fonts/',
